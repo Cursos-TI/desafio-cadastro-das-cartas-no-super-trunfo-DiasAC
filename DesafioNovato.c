e1bf9f0c2a85c2c36a0a0a5a8f612111e8e34ab2
DiasAC <acdias.96@outlook.com>
2025-03-13 00:28:41 +0000

    Inseri dados primitivos para criar novos dados

--- DesafioNovato.c
+++ DesafioNovato.c
@@ -3,7 +3,15 @@
     int main(){
      printf("Desafio Super Trunfo\n");
      
+     //Carta 1
+     int populacao, ponto_turistico;
+     float pib, area_km;
+     char estado[3];
+     char codigo_carta[5];
+     char cidade [40];
 
+    printf("Digite o codigo do Estado: \n");
+    scanf("%d" , &estado);  
 
 
 
